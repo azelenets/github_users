33d32176037fbd865e1d14d93df12d4edcaa1292
Andrew Zelenets <andrew.zelenets@gmail.com>
2020-02-11 17:41:56 +0200

    use postgresql as database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_10_134654) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +26,5 @@ ActiveRecord::Schema.define(version: 2020_02_10_134654) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
